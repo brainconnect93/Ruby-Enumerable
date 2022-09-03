147a48e9932a5c612ad0b64e049a28ca74fd1c8f
t-bello7 <bello4aus@gmail.com>
2022-09-03 13:34:27 +0100

    create module and classes|

--- my_list.rb
+++ my_list.rb
@@ -1,34 +1,11 @@
+require_relative './my_enumerable'
 class MyList
   include MyEnumerable
-    def initialize(*list)
-        @list = list
-    end
+  def initialize(list)
+    @list = list
+  end
 
-    def each(&value)
-        @list.each(&value)
-    end
+  def each(&block)
+    @list.each { |y| block.call(y) }
+  end
 end
-
-list = MyList.new(1, 2, 3, 4, 5, 6, 7)
-
-# the test all function
-puts(list.all? { |e| e < 8})
-puts(list.all? { |e| e > 8})
-
-# the test any function
-puts(list.any? { |e| e == 2})
-puts(list.any? {|e| e == 8})
-
-# the test filter function 
-puts(list.filter? { |e| e.even})
-
-
-# class Person 
-#     def initialize( name ) 
-#          @name = name
-#     end
-
-#     def do_with_name   # expects a block
-#         yield( @name ) # invoke the block and pass the `@name` attribute
-#     end
-# end
